3556a87f8b598d4b5c4f2c088753310d111ec27f
Jason Li <jasonmli@outlook.com>
2025-08-13 23:23:10 +0800

    build(android): 更新 NDK 版本并增加应用版本号
    - 将 NDK 版本从 flutter.ndkVersion 更新为 "27.0.12077973"
    - 在 pubspec.yaml 中将应用版本号从 0.0.1+4 更新为 0.0.1+5

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 android {
     namespace = "com.cup11.unison"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -23,7 +23,6 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.cup11.unison"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
